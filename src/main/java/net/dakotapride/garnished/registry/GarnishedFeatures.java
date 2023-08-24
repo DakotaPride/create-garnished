17ea65ab3dafba274bfb0c1990bdf0d26d799137
DakotaPride <rjtube13@gmail.com>
2023-08-24 14:36:10 -0500

    0.6 - Nutrees

--- src/main/java/net/dakotapride/garnished/registry/GarnishedFeatures.java
+++ src/main/java/net/dakotapride/garnished/registry/GarnishedFeatures.java
@@ -31,8 +31,8 @@ public class GarnishedFeatures {
 			FeatureUtils.register(CreateGarnished.ID + ":nut_tree_configured", Feature.TREE,
 					new TreeConfiguration.TreeConfigurationBuilder(BlockStateProvider.simple(Blocks.OAK_LOG),
 							new BendingTrunkPlacer(4, 2, 0, 3, UniformInt.of(1, 2)),
-							new WeightedStateProvider(SimpleWeightedRandomList.<BlockState>builder().add(Blocks.SPRUCE_LEAVES.defaultBlockState(), 3)
-									.add(Blocks.AZALEA_LEAVES.defaultBlockState(), 1)),
+							new WeightedStateProvider(SimpleWeightedRandomList.<BlockState>builder().add(Blocks.OAK_LEAVES.defaultBlockState(), 3)
+									.add(GarnishedBlocks.NUT_LEAVES.get().defaultBlockState(), 1)),
 							new RandomSpreadFoliagePlacer(ConstantInt.of(3), ConstantInt.of(0), ConstantInt.of(2), 50),
 							new TwoLayersFeatureSize(1, 0, 1)).dirt(BlockStateProvider.simple(Blocks.DIRT)).forceDirt().build());
 
