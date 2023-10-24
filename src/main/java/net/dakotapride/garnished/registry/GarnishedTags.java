f4373be4620c58ee275c3407ee55079a1a294d06
DakotaPride <rjtube13@gmail.com>
2023-10-24 10:41:05 -0500

    v1.2 - misc fixes

--- src/main/java/net/dakotapride/garnished/registry/GarnishedTags.java
+++ src/main/java/net/dakotapride/garnished/registry/GarnishedTags.java
@@ -1,12 +1,9 @@
 package net.dakotapride.garnished.registry;
 
 import net.dakotapride.garnished.CreateGarnished;
-import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
@@ -15,30 +12,30 @@ import net.minecraft.world.level.material.Fluid;
 public class GarnishedTags {
 
 	public static TagKey<Item> AVERSION_FOODS_TAG = itemTag("aversion_foods");
-	public static TagKey<Item> HATCHETS_TAG = forgeItemTag("tools/hatchets");
+	public static TagKey<Item> HATCHETS_TAG = commonItemTag("tools/hatchets");
 	public static TagKey<Fluid> GARNISHED_FLUIDS_TAG = fluidTag("fluids");
-	public static final TagKey<Block> MINEABLE_WITH_HATCHET = forgeBlockTag("mineable/hatchet");
-	public static final TagKey<EntityType<?>> IS_AFFECTED_BY_SALVAGING = forgeEntityTypeTag("affected_by/salvaging");
-	public static final TagKey<EntityType<?>> IS_AFFECTED_BY_RAVAGING = forgeEntityTypeTag("affected_by/ravaging");
+	public static final TagKey<Block> MINEABLE_WITH_HATCHET = commonBlockTag("mineable/hatchet");
+	public static final TagKey<EntityType<?>> IS_AFFECTED_BY_SALVAGING = commonEntityTypeTag("affected_by/salvaging");
+	public static final TagKey<EntityType<?>> IS_AFFECTED_BY_RAVAGING = commonEntityTypeTag("affected_by/ravaging");
 
 	private static TagKey<Item> itemTag(String name) {
 		return TagKey.create(BuiltInRegistries.ITEM.key(), new ResourceLocation(CreateGarnished.ID, name));
 	}
 
-	private static TagKey<Item> forgeItemTag(String name) {
-		return TagKey.create(BuiltInRegistries.ITEM.key(), new ResourceLocation("forge", name));
+	private static TagKey<Item> commonItemTag(String name) {
+		return TagKey.create(BuiltInRegistries.ITEM.key(), new ResourceLocation("c", name));
 	}
 
-	private static TagKey<Block> forgeBlockTag(String name) {
-		return TagKey.create(BuiltInRegistries.BLOCK.key(), new ResourceLocation("forge", name));
+	private static TagKey<Block> commonBlockTag(String name) {
+		return TagKey.create(BuiltInRegistries.BLOCK.key(), new ResourceLocation("c", name));
 	}
 
 	private static TagKey<Fluid> fluidTag(String name) {
 		return TagKey.create(BuiltInRegistries.FLUID.key(), new ResourceLocation(CreateGarnished.ID, name));
 	}
 
-	private static TagKey<EntityType<?>> forgeEntityTypeTag(String name) {
-		return TagKey.create(BuiltInRegistries.ENTITY_TYPE.key(), new ResourceLocation("forge", name));
+	private static TagKey<EntityType<?>> commonEntityTypeTag(String name) {
+		return TagKey.create(BuiltInRegistries.ENTITY_TYPE.key(), new ResourceLocation("c", name));
 	}
 
 	public static void setRegister() {}
