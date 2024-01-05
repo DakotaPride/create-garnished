f1d5cc21f75913486b7145af7787b8606c78641f
DakotaPride <rjtube13@gmail.com>
2024-01-04 19:38:39 -0600

    v1.5 - last minute additions

--- src/main/java/net/dakotapride/garnished/block/VoltaicSeagrassBlock.java
+++ src/main/java/net/dakotapride/garnished/block/VoltaicSeagrassBlock.java
@@ -4,6 +4,9 @@ import net.dakotapride.garnished.registry.GarnishedBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
@@ -12,12 +15,29 @@ import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.SeagrassBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
+
 import org.jetbrains.annotations.NotNull;
 
 public class VoltaicSeagrassBlock extends SeagrassBlock {
     public VoltaicSeagrassBlock(Properties properties) {
         super(properties);
     }
+
+	@Override
+	public void entityInside(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull Entity entity) {
+		if (entity instanceof LivingEntity) {
+			entity.makeStuckInBlock(state, new Vec3(0.800000011920929, 0.75, 0.800000011920929));
+			if (!level.isClientSide && (entity.xOld != entity.getX() || entity.zOld != entity.getZ())) {
+				double d = Math.abs(entity.getX() - entity.xOld);
+				double e = Math.abs(entity.getZ() - entity.zOld);
+				if (d >= 0.003000000026077032 || e >= 0.003000000026077032) {
+					entity.hurt(entity.damageSources().lightningBolt(), 1.0F);
+				}
+			}
+
+		}
+	}
 
     @Override
     public @NotNull ItemStack getCloneItemStack(@NotNull BlockGetter level, @NotNull BlockPos pos, @NotNull BlockState state) {
